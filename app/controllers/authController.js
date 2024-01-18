511fe51b15b5e0d9c873c38a59430f83b53d0d80
WilliamCommandeur <william.commandeur@gmail.com>
2024-01-18 17:44:59 +0100

    add register form

---
+++ app/controllers/authController.js
@@ -0,0 +1,9 @@
+const authController = {
+
+    registerForm(req, res) {
+        res.render('signup')
+    }
+
+};
+
+module.exports = authController;
--- app/router.js
+++ app/router.js
@@ -1,9 +1,9 @@
-const { cp } = require("fs");
-
 const router = require('express').Router();
+const appController = require('./controllers/appController');
+const authController = require('./controllers/authController');
 
-router.get('/', (req, res) => {
-    res.render('index');
-});
+router.get('/', appController.index);
+
+router.get('/register', authController.registerForm);
 
 module.exports = router;
