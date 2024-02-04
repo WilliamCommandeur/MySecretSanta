b1739a4ac2740514e370057f0d3bc0b8d935fb6d
WilliamCommandeur <william.commandeur@gmail.com>
2024-02-04 19:15:38 +0100

    add the draw function

--- test.js
+++ test.js
@@ -1,18 +1,15 @@
 const participants = ['William', 'Albert', 'Jean', 'Marie', 'Anne'];
-
-
+// Je mélange aléatoirement les éléments du tableau
+const shuffledParticipants = participants.sort(() => Math.random() - 0.5);
 
 function draw(name) {
-    const newArray = participants.filter((participant) => participant !== name )
-    
-    
-    const result = newArray[0]
-    
-    return result
-};
+    // Pour chaque participant, je choisis le participant qui se situe à l'index suivant
+    // Si le résultat est undefined, celà veut dire que le participant est le dernier élément du tableau
+    // Dans ce cas je lui donne le premier élément du tableau
+    const resultOfDraw = shuffledParticipants[shuffledParticipants.indexOf(name) + 1] ?? shuffledParticipants[0];
+    return resultOfDraw;
+}
 
-
-
-const test = draw('William');
-
-console.log(test);
+for (participant of participants) {
+    draw(participant);
+}
